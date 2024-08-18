df3be80d3e3d4608afc484be90ad311a4504d2b3
Raftaly <ethan.lim.free.fr@gmail.com>
2024-08-18 11:08:07 +0200

    Reconstruction du module UI

--- UI.py
+++ UI.py
@@ -2,10 +2,39 @@ from FiveM import FiveM
 from Minecraft import Minecraft
 from SCP import SCP
 
-import threading
-
 import tkinter
-import tkinter.ttk as ttk
+from tkinter import ttk
 
-MAX_BOUTONS_MENU_PAR_LIGNE = 2
+class FormulaireAcces:
+    def __init__(self,nom_jeu,root,n_colonne):
+        self.jeu = nom_jeu
+        self.colonne_frame = ttk.Frame(root)
+        
+        titre_colonne = ttk.Label(self.colonne_frame,text=nom_jeu)
+        titre_colonne.grid(row=0)
 
+        self.entrees = {}
+
+        ligne = 1
+        for info in ["Pseudo","IP","Mot de passe"] :
+            entree =  ttk.Entry(self.colonne_frame)
+            entree.insert(tkinter.END,info)
+            entree.grid(row=ligne)
+            self.entrees[info] = entree
+
+            ligne += 1
+
+        self.bouton_valider = ttk.Button(self.colonne_frame,text="Vérifier les informations")
+        self.bouton_valider.grid(row=ligne)
+
+        self.colonne_frame.grid(column=n_colonne)
+
+    def verifie_infos(self):
+        if self.jeu == "FiveM" :
+            client = FiveM(self.entrees["IP"].get(),self.entrees["Pseudo"].get(),self.entrees["Mot de passe"].get())
+        elif self.jeu == "Minecraft" :
+            client = Minecraft(self.entrees["IP"].get(),self.entrees["Pseudo"].get(),self.entrees["Mot de passe"].get()) 
+        elif self.jeu == "SCP:SL" :
+            client = SCP(self.entrees["IP"].get(),self.entrees["Pseudo"].get(),self.entrees["Mot de passe"].get())
+        
+    
